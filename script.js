a5c9c72bc22ee41e8c92a95a541b3aadab4f7142
Yayooshka <yayooshka@mail.ru>
2022-06-29 18:52:51 +0300

    lessone6

--- script.js
+++ script.js
@@ -1,92 +1,92 @@
-const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
-];
-
 const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
 const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
 
-function service(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onload = () => {
-    if (xhr.readyState === 4) {
-      callback(JSON.parse(xhr.response))
-    }
-  }
+function service(url) {
+  return fetch(url)
+  .then((res) => res.json())
 }
 
-class GoodsItem {
-  constructor({ product_name, price }) {
-    this.product_name = product_name;
-    this.price = price;
-  }
-  render() {
-    return `
+function init() {
+  const CustomButton = Vue.component('custom-button', {
+      template: `
+      <button class="search-button" type="button" v-on:click="$emit('click')">
+      <slot></slot>
+      </button>
+      `
+  })
+  const basketGoods = Vue.component('basket-goods', {
+    data() {
+      return {
+        basketGoodsItems: []
+      }
+    },
+
+    template: `
+    <div class="fixed-area">
+      <div class="basket-card">
+        <div class="basket-card__header">
+          <h1 class="basket-card__header__title">basket card</h1>
+          <div class="basket-card__header__delete-icon"
+            v-on:click="$emit('closeclick')">
+          </div>
+        </div>
+        <div class="basket-card__content">
+          content
+        </div>
+      </div>
+    </div>
+    `
+  })
+
+  const goodsItem = Vue.component('goods-item', {
+    props: [
+      'item'
+    ],
+    template: `
     <div class="goods-item">
-      <h3>${this.product_name}</h3>
-      <p>${this.price}</p>
-      </div>`;
-  }
-}
+      <h3>{{ this.product_name }}</h3>
+      <p>{{ this.price }}</p>
+    </div>
+    `
+  })
 
-class GoodsList {
-  items = [];
-  filteredItems = [];
-  fetchGoods(callback) {
-    service(GET_GOODS_ITEMS, (data) => {
-      this.items = data;
-      this.filteredItems = data;
-      callback()
-    });
-  }
+  const app = new Vue({
+    el: '#root',
+    data: {
+      items: [],
+      filteredItems: [],
+      search: '',
+      cardIsVision: false
+    },
+    methods: {
+      setVisionCard() {
+        this.cardIsVision = !this.cardIsVision
+      },
+      fetchGoods() {
+        service(GET_GOODS_ITEMS).then((data) => {
+          this.items = data;
+          this.filteredItems = data;
+        });
+      },
+      filterItems() {
+        this.filteredItems = this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.search, 'gui'))
+        })
+      },
+    },
+    computed: {
+      calcucatePrice() {
+       return this.filteredItems.reduce((prev, { price }) => {
+          return prev + price;
+        }, 0)
+      }
+    },
 
-  filterItems(value) {
-    this.filteredItems = this.items.filter(({product_name}) => {
-      return product_name.match(new RegExp(value, 'gui'))
-    })
-  }
-
-  calcucatePrice() {
-    return this.items.reduce((prev, {price}) => {
-      return prev + price;
-     }, 0)
-  }
-  
-  render() {
-    const goods = this.filteredItems.map(item => {
-      const goodItem = new GoodsItem(item);
-      return goodItem.render()
-    }).join('');
-
-    document.querySelector('.goods-list').innerHTML = goods;
+    mounted() {
+      this.fetchGoods();
     }
-  }
-
-
-class BasketGoods {
-  items = [];
-  fetchGoods() {
-    service(GET_BASKET_GOODS_ITEMS, (data) => {
-      this.items = data.contents;
-    })
-  }
+  })
 }
 
-const basketGoods = new BasketGoods();
-basketGoods.fetchGoods();
-
-const goodsList = new GoodsList();
-goodsList.fetchGoods(() => {
-  goodsList.render();
-});
-
-document.getElementsByClassName('search-button')[0].addEventListener('click', () => {
-  const value = document.getElementsByClassName('goods-search')[0].value;
-  goodsList.filterItems(value);
-  goodsList.render();
-})  
+window.onload = init
